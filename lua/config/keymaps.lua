9c70f01db295ac67d6d8b29960840ff59525d867
fengjie <fjie666@outlook.com>
2024-08-30 09:19:18 +0800

    add plugin neoscroll

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,17 +9,33 @@ end
 
 -- 普通模式下的键映射
 vim.api.nvim_set_keymap('n', 'L', 'g_', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'dL', 'dG_', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '0', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'dL', 'dg_', { noremap = true, silent = true })
 
 -- 可视模式下的键映射
 vim.api.nvim_set_keymap('v', 'L', 'g_', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', 'dL', 'dG_', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'dL', 'dg_', { noremap = true, silent = true })
+
+
+vim.api.nvim_set_keymap('n', '0', '^', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '0', '^', { noremap = true, silent = true })
 
 -- 禁用某些按键
 vim.api.nvim_set_keymap('n', 'H', ':lua echoDisabled("H")<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'J', ':lua echoDisabled("J")<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'L', ':lua echoDisabled("L")<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'U', ':lua echoDisabled("U")<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'P', ':lua echoDisabled("P")<CR>', { noremap = true, silent = true })
+
+neoscroll = require('neoscroll')
+
+local keymap = {
+  ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end;
+  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end;
+  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 250 }) end;
+  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 250 }) end;
+}
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
+end
 
