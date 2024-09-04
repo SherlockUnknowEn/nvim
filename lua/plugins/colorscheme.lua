33130da9dc74e9a94fb6030b40ea82a06b5ed739
冯了个杰 <fjie666@outlook.com>
2024-09-04 23:36:05 +0800

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,25 @@
 return {
-    { "echasnovski/mini.icons", config = { style = 'ascii' } }
+    { "echasnovski/mini.icons", config = { style = "ascii" } },
+    {
+        "navarasu/onedark.nvim",
+        config = {
+            style = "dark",
+            colors = {
+                highlight_red = "#ef5f6b",
+            },
+            highlights = {
+                FlashLabel = { fg = "$highlight_red" },
+                TelescopeMatching = { fg = "$highlight_red", bold = true },
+                DashboardHeader = { fg = "orange", bold = true },
+                DashboardFooter = { fg = "#e5c07b", italic = true },
+                DashboardKey = { fg = "#ff966c", bold = true }
+            }
+        }
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "onedark",
+        },
+    },
 }
